ddd1b8ac7605224a5431cedb7a7abe6f681fba1b
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-08 14:01:10 +0530

    Progress on 8th Feb 2023

---
+++ Basic JavaScript/Build JavaScript Objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name":"Moti",
+    "legs":4,
+    "tails":1,
+    "friends":["Bella","Max","Luna","Charlie","Lucy"]
+    // Only change code above this line
+  };
