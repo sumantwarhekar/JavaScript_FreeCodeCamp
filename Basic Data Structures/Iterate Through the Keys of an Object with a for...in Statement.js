382c8703f6e74d9053838937ca5879c09ceebbed
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-15 19:20:30 +0530

    15th Feb : Basic Data Structures Completed

---
+++ Basic Data Structures/Iterate Through the Keys of an Object with a for...in Statement.js
@@ -0,0 +1,25 @@
+const users = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+  
+  function countOnline(usersObj) {
+    // Only change code below this line
+    let count=0;
+    for(let i in usersObj){
+      if(usersObj[i]["online"]==true){
+        count++;
+      }
+    }
+    return count;
+    // Only change code above this line
+  }
+  
+  console.log(countOnline(users));
