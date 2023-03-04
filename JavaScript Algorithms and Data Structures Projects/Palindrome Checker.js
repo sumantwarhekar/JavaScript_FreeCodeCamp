15a82f4c6b85c9853cdbbb05e1b7f2a0910848a1
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-03-05 02:20:37 +0530

    Progress on 5th Mar 2023

---
+++ JavaScript Algorithms and Data Structures Projects/Palindrome Checker.js
@@ -0,0 +1,18 @@
+function palindrome(str) {
+    let cleanStr="";
+    let lower = str.toLowerCase();
+    for(let i=0;i<lower.length;i++){
+      if((lower[i]>="a"&&lower[i]<="z")||(lower[i]>="1"&&lower[i]<="9")){
+        cleanStr+=lower[i]
+      }
+    }
+    let reverse=cleanStr.split("").reverse().join("");
+    console.log(cleanStr)
+    console.log(reverse)
+    if(cleanStr===reverse)
+      return true;
+    else
+      return false;
+  }
+  
+  palindrome("1 eye for of 1 eye.");
